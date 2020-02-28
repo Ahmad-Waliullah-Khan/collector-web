269e363cf4d51f9f5e1799c15023be8e5fa7cf3a
Ahmad <ahmad.waliullah.khan@gmail.com>
2020-02-28 19:22:45 +0530

    - Login fix. - Working on Dashboard.

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,6 @@ app_name = 'home'
 urlpatterns = [
     path('', views.login, name='login'),
     path('profile/', views.profile, name='profile'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path("logout/", views.logout, name="logout"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,27 +1,31 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login as auth_login
 
 def login(request):
-    if not request.user.is_authenticated:
         if request.method == 'POST':
             username = request.POST['username']
             password =  request.POST['password']
             post = User.objects.filter(username=username)
             if post:
                 username = request.POST['username']
-                user = authenticate(username=username, password=password)
-                if user is not None:
+                user = authenticate(request, username=username, password=password)
+                if user:
+                    auth_login(request, user)
                     request.session['username'] = username
-                    return redirect("home:profile")
+                    return redirect("home:dashboard")
                 else:
                     return render(request, 'home/login.html', {})
             else:
                 return render(request, 'home/login.html', {})
-    else:
-        return redirect("home:profile")
-    return render(request, 'home/login.html', {})
+        else:
+            if request.user.is_authenticated:
+                return redirect("home:dashboard")
+        return render(request, 'home/login.html', {})
 
+@login_required
 def profile(request):
     if request.session.has_key('username'):
         posts = request.session['username']
@@ -29,3 +33,17 @@ def profile(request):
         return render(request, 'home/profile.html', {"query":query})
     else:
         return render(request, 'home/login.html', {})
+
+@login_required
+def dashboard(request):
+    if request.user.is_authenticated:
+
+        return render(request, 'home/dashboard.html', {})
+    else:
+        return redirect("home:login")
+
+@login_required
+def logout(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("home:login")
